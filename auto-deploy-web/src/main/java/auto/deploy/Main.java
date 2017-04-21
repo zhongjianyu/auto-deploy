cf746f975a86dfe02a00ddadc030f58f1ab7041b
zhongjy <zhongjy@PC-zhongjy>
2017-04-21 09:00:13 +0800

    add

--- auto-deploy-web/src/main/java/auto/deploy/Main.java
+++ auto-deploy-web/src/main/java/auto/deploy/Main.java
@@ -3,6 +3,14 @@ package auto.deploy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 
+ * @描述：项目启动类
+ *
+ * @作者：zhongjy
+ *
+ * @时间：2017年4月21日 上午8:40:51
+ */
 @SpringBootApplication
 public class Main {
 
