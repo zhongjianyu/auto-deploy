446da95ea95aad0df5cce5f83e54072dc442248b
zhongjy <zhongjy@zhongjy-pc>
2017-04-21 22:36:21 +0800

    add

--- auto-deploy-web/src/main/java/auto/deploy/web/Main.java
+++ auto-deploy-web/src/main/java/auto/deploy/web/Main.java
@@ -1,4 +1,4 @@
-package auto.deploy;
+package auto.deploy.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- auto-deploy-web/src/main/java/auto/deploy/web/controller/IndexController.java
+++ auto-deploy-web/src/main/java/auto/deploy/web/controller/IndexController.java
@@ -1,4 +1,4 @@
-package auto.deploy.controller;
+package auto.deploy.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
