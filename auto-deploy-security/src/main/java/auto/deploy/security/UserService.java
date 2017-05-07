d9520e89b7a4e6982b5fa4bded8659d1c4f4613a
zhongjy <zhongjy@192.168.0.106>
2017-05-07 16:47:48 +0800

    add

--- auto-deploy-security/src/main/java/auto/deploy/security/UserService.java
+++ auto-deploy-security/src/main/java/auto/deploy/security/UserService.java
@@ -3,10 +3,17 @@ package auto.deploy.security;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.Resource;
+
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
+
+import auto.deploy.dao.entity.AutUser;
 import auto.deploy.object.vo.AutRoleVO;
 import auto.deploy.object.vo.AutUserVO;
+import auto.deploy.service.authority.AutUserService;
 
 /**
  * 
@@ -19,14 +26,23 @@ import auto.deploy.object.vo.AutUserVO;
 @Service
 public class UserService {
 
+	@Resource
+	private AutUserService autUserService;
+
+	/**
+	 * 
+	 * @描述：根据登录名获取用户
+	 *
+	 * @返回：AutUserVO
+	 *
+	 * @作者：zhongjy
+	 *
+	 * @时间：2017年5月7日 下午3:51:32
+	 */
 	public AutUserVO getUserByUserName(String userName) {
 		AutUserVO user = new AutUserVO();
-		user.setUserName("zjy");
-		user.setUserPwd("1234");
-		user.setIsActive(1);
-		user.setIsAccountExpired(0);
-		user.setIsAccountLocked(0);
-		user.setIsCredentialsExpired(0);
+		AutUser autUser = autUserService.selectOne(new EntityWrapper<AutUser>().eq("user_name", userName));
+		BeanUtils.copyProperties(autUser, user);
 		List<AutRoleVO> roleList = new ArrayList<AutRoleVO>();
 		AutRoleVO role1 = new AutRoleVO();
 		role1.setRoleCode("sadmin");
