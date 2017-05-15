2e5279ed6c926bdb65350bf733c4d37d4a79d83a
zhongjy <zhongjy@PC-zhongjy>
2017-05-15 19:46:54 +0800

    add

---
+++ auto-deploy-redis/src/main/java/auto/deploy/Main.java
@@ -0,0 +1,32 @@
+package auto.deploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * @描述：项目启动类
+ *
+ * @作者：zhongjy
+ *
+ * @时间：2017年4月21日 上午8:40:51
+ */
+@SpringBootApplication
+public class Main {
+
+	/**
+	 * 
+	 * @描述：项目启动入口方法
+	 *
+	 * @返回：void
+	 *
+	 * @作者：zhongjy
+	 *
+	 * @时间：2017年4月24日 下午12:29:53
+	 */
+	public static void main(String[] args) {
+		SpringApplication application = new SpringApplication(Main.class);
+		application.run(args);
+	}
+
+}
