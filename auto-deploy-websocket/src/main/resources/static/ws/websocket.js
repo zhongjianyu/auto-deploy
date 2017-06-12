47f18c0761842c291dc55dfa2ec4c32deca1750e
zhongjianyu <949434438@qq.com>
2017-06-12 23:14:43 +0800

    add

--- auto-deploy-websocket/src/main/resources/static/ws/websocket.js
+++ auto-deploy-websocket/src/main/resources/static/ws/websocket.js
@@ -1,8 +1,11 @@
 var stompClient = null;
 /**
  * 建立连接
+ * 
+ * @param topic--广播回调
+ * @param point2point--点对点回调
  */
-function connect() {
+function connect(topic, point2point) {
 	if (stompClient != null) {
 		stompClient.disconnect();
 	}
@@ -11,17 +14,17 @@ function connect() {
 	// 使用stomp子协议的WebSocket 客户端
 	stompClient = Stomp.over(socket);
 	// 链接Web Socket的服务端
-	stompClient.connect("1", "2", function(frame) {
+	stompClient.connect({}, function(frame) {
 		// console.log('连接已建立: ' + frame);
 		// 订阅/topic/getResponse
 		stompClient.subscribe('/topic/getResponse', function(respnose) {
 			// 消息推送响应(广播式)
-			console.log(JSON.parse(respnose.body));
+			topic(JSON.parse(respnose.body));
 		});
 		// 订阅/user/point2point/getResponse
 		stompClient.subscribe('/user/point2point/getResponse', function(respnose) {
 			// 消息推送响应(点对点)
-			console.log(JSON.parse(respnose.body).message);
+			point2point(JSON.parse(respnose.body));
 		});
 	});
 }
